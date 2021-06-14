be12e43319216e9807122701bf00fb7bc442345f
root <root@instance-1.us-central1-a.c.neat-coast-316300.internal>
2021-06-14 02:10:50 +0000

    Updates

--- Administration/Generate_Cluster-User/add_k8_cluster_user.sh
+++ Administration/Generate_Cluster-User/add_k8_cluster_user.sh
@@ -17,8 +17,8 @@ sudo cp /etc/kubernetes/pki/ca.{crt,key} .
 openssl x509 -req -in $1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out $1.crt -days 365
 
 
-kubectl --kubeconfig $1.kubeconfig config set-cluster kuberenets --server  https://10.128.0.8:6443  --certificate-authority ca.crt --embed-certs=true
-kubectl --kubeconfig $1.kubeconfig config set-credentials $1 --client-certificate /root/$1/$1.crt --client-key /root/$1/$1.key --embed-certs=true
+kubectl --kubeconfig $1.kubeconfig config set-cluster kuberenets --server $k8server  --certificate-authority ca.crt --embed-certs=true
+kubectl --kubeconfig $1.kubeconfig config set-credentials $1 --client-certificate $PWD/$1.crt --client-key $PWD/$1.key --embed-certs=true
 kubectl --kubeconfig $1.kubeconfig config set-context $1-kubernetes --cluster kuberenets --namespace $2 --user $1
 kubectl --kubeconfig $1.kubeconfig config use-context $1-kubernetes
 
